eb0ded7a16fe69c8170ed8339d104e1f9f24e6a8
Ricardop926 <ricardo.porras26@outlook.com>
2022-08-05 19:06:08 -0500

    se hizo el login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public myForm!:FormGroup;
 
-  ngOnInit(): void {
+  constructor(private fb:FormBuilder) { }
+
+ ngOnInit(): void {
+  
+  this.myForm = this.createMyForm();
   }
 
+private createMyForm():FormGroup {
+
+  return this.fb.group({
+
+    usuario:['', [Validators.required]],
+    password:['', Validators.required]
+  });
 }
+
+public submitFormulario() {
+  
+  if(this.myForm.invalid)
+  {
+    Object.values(this.myForm.controls).forEach(control=>{
+      control.markAllAsTouched();
+    });
+    return;
+  }
+   console.log(this.myForm.value);
+ 
+}
+
+public get f():any{
+  return this.myForm.controls;
+}
+}
+ 
